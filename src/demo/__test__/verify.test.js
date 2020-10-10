7bf34b842d2990441440ece5c391dec21e0a3cad
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-10-10 13:11:43 +0800

    [mengqiu] finish test writting for /demo

--- src/demo/__test__/verify.test.js
+++ src/demo/__test__/verify.test.js
@@ -4,9 +4,12 @@ describe("verifyPassword", () => {
   test("should return false when password length < 6", () => {
     // Arrange
     // 设置输入密码password的值为“12345”
+    const password = "12345";
     // Act
     // 调用验证密码函数 verifyPassword(password)
+    const result = verifyPassword(password);
     // Assert
     // 验证输出结果是否为false
+    expect(result).toBe(false);
   });
 });
