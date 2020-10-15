b64b8d972a6926eac1979bc26e8a73d3662df3a5
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-10-15 23:55:04 +0800

    [mengqiu] delete already done test for module.manual

--- src/mock/module.manual/__test__/user.test.js
+++ src/mock/module.manual/__test__/user.test.js
@@ -7,12 +7,6 @@ describe("validate", () => {
   test("should validate username and password", () => {
     // Arrange
     // Act
-    const username = "mocked username";
-    const password = "mocked password";
-    const result = validate(username, password);
     // Assert
-    expect(verifyPassword).toHaveBeenCalledWith(password);
-    expect(verifyUsername).toHaveBeenCalledWith(username);
-    expect(result).toBeTruthy();
   });
 });
