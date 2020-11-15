fd27db5fb5b594bb6bdaeb9edd03395c1f9a00b5
wmxl <294036748@qq.com>
2020-11-15 21:11:53 +0800

    [yiming] add 2 test

--- src/mock/function/__test__/filter.test.js
+++ src/mock/function/__test__/filter.test.js
@@ -3,8 +3,16 @@ import filter from "../filter";
 describe("mock callback", () => {
   test("toHaveBeenCalledWith", () => {
     // Arrange
+    const arr = [1, 2, 3, 4];
+    const callback = jest.fn();
     // Act
+    callback
+      .mockReturnValue(false) // globe
+      .mockReturnValueOnce(true) // first
+      .mockReturnValueOnce(true); // second
+    const res = filter(callback, arr);
     // Assert
+    expect(res).toEqual([1, 2]);
   });
 
   test("toHaveReturned", () => {
--- src/mock/module.auto/__test__/user.test.js
+++ src/mock/module.auto/__test__/user.test.js
@@ -1,10 +1,16 @@
 import { verifyPassword, verifyUsername } from "../verify";
 import validate from "../user";
 
+jest.mock("../verify");
+
 describe("user", () => {
   test("should validate username and password", () => {
     // Arrange
     // Act
+    verifyPassword.mockReturnValue(true);
+    verifyUsername.mockReturnValue(true);
     // Assert
+    const res = validate("aaa", "bbb");
+    expect(res).toEqual(true);
   });
 });
